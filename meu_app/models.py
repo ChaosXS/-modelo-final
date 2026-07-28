229416afcb4d714cae21cf7d6d5f31126fa84771
Seu Nome <laisaeflur@gmail.com>
2026-07-28 19:23:44 -0300

    Adicionando os modelos ao projeto

--- meu_app/models.py
+++ meu_app/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
+from django.db import models
+
 class Livro(models.Model):
     titulo = models.CharField(max_length=100)
     autor = models.CharField(max_length=50)
     ano_publicacao = models.IntegerField()
+    
